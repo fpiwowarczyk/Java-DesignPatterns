c247844e73c8238da8f59feb0738f28e80b8dcac
filip.piwowarczyk <filip.piwowarczyk@palturai.com>
2021-02-19 13:49:05 +0100

    Init

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String name1 = "Muller GmbH";
+        String name2 = name1.toLowerCase();
+	System.out.println(name2);
+    }
+}
