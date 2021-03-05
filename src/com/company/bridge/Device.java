c7422ba724dafbc58cc3f7a873ab8b6db5af2447
filip.piwowarczyk <filip.piwowarczyk@palturai.com>
2021-03-05 14:42:12 +0100

    ADd birdge

---
+++ src/com/company/bridge/Device.java
@@ -0,0 +1,19 @@
+package com.company.bridge;
+
+public interface Device {
+    boolean isEnables();
+
+    void enable();
+
+    void disable();
+
+    int getVolume();
+
+    void setVolume(int percent);
+
+    int getChannel();
+
+    void setChannel(int channel);
+
+    void printStatus();
+}
---
+++ src/com/company/bridge/README.md
@@ -0,0 +1,3 @@
+# Bridge 
+Birdge is a structural design pattern that lets you split a large class or a set of closely related classes into two separated hierarchies - 
+abstraction and implementation - which can be developed independently of each other. 
